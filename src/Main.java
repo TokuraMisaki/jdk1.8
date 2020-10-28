c5fbcf1c7c71b93f4a6087f7b4af684583f47d8b
zhouyang <1251341510@qq.com>
2020-10-28 11:34:08 +0800

    创建jdk1.8源码阅读环境

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Descrption: Main
+ * @Author: HC
+ * @Date: 2020/10/28 11:17
+ */
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(1,1);
+        map.put(2,1);
+        map.put(3,1);
+        map.put(4,1);
+        map.put(5,1);
+        map.put(6,1);
+        map.put(7,1);
+        map.put(8,1);
+        map.put(9,1);
+        map.put(10,1);
+    }
+}
